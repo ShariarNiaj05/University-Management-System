cc41f85148d66015975a7b0c5145b523ede103b5
Shariar Islam <shariarn85@gmail.com>
2024-05-25 00:49:57 +0600

    Create Index Route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Application } from 'express';
 import cors from 'cors';
-import { UserRoutes } from './app/modules/users/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundRoute from './app/middlewares/notFoundRoute';
+import router from './app/routes';
 const app: Application = express();
 // const port = process.env.PORT;
 
@@ -12,11 +12,12 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/users/', UserRoutes);
+app.use('/api/v1', router);
 
 // global error handler route
 app.use(globalErrorHandler);
 
 // not found route
 app.use(notFoundRoute);
+
 export default app;
