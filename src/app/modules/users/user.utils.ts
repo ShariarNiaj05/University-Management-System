6f515f3cc11ff9b984c9f7db6ef770c780e2ae1a
Shariar Islam <shariarn85@gmail.com>
2024-05-27 01:11:26 +0600

    Complete GenerateStudent() Utility Function

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -1,13 +1,31 @@
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import User from './user.model';
 
-export const generateStudentId = (payload: TAcademicSemester) => {
+const findLastStudentId = async () => {
+  const lastStudent = await User.findOne(
+    { role: 'student' },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+  // 203001 0001
+  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+};
+
+export const generateStudentId = async (payload: TAcademicSemester) => {
   /* 
     (0).toString().padStart(4, '0') == "0000"
     (10).toString().padStart(4, '0') == "0010"
     (16).toString().padStart(4, '0') == "0016"
     (118).toString().padStart(4, '0') == "0118"
     */
-  const currentID = (0).toString();
+
+  const currentID = (await findLastStudentId()) || (0).toString();
   let incrementID = (Number(currentID) + 1).toString().padStart(4, '0');
 
   incrementID = `${payload.year}${payload.code}${incrementID}`;
