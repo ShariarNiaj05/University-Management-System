dcdc244d7175121022e1d1bb509883c424eec2ed
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-03 23:45:54 +0600

    Create Routes

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,5 +1,19 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { CourseValidations } from './course.validation';
+import { CourseControllers } from './course.controller';
 
 const router = express.Router();
+
+router.post(
+  '/create-course',
+  validateRequest(CourseValidations.createCourseValidationSchema),
+  CourseControllers.createCourse,
+);
+
+router.post('/:id', CourseControllers.getSingleCourse);
+router.delete('/:id', CourseControllers.deleteCourse);
+
+router.get('/', CourseControllers.getAllCourses);
 
 export const CourseRoutes = router;
