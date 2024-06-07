df98afefddbb23bb5c8a2e87629901eff3c78d13
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-07 11:12:24 +0600

    service, controller and route added for offered course

---
+++ src/app/modules/OfferedCourse/OfferedCourse.services.ts
@@ -0,0 +1,32 @@
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    section,
+    faculty,
+    days,
+    startTime,
+    endTime,
+  } = payload;
+};
+
+const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {};
+
+const getSingleOfferedCourseFromDB = async (id: string) => {};
+
+const updateOfferedCourseIntoDB = async (
+  id: string,
+  payload: Pick<TOfferedCourse, 'faculty' | 'days' | 'startTime' | 'endTime'>,
+) => {};
+
+const deleteOfferedCourseFromDB = async (id: string) => {};
+
+export const OfferedCourseServices = {
+  createOfferedCourseIntoDB,
+  getAllOfferedCoursesFromDB,
+  getSingleOfferedCourseFromDB,
+  deleteOfferedCourseFromDB,
+  updateOfferedCourseIntoDB,
+};
