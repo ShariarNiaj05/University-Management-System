3d08761e5c3bb98d902c67249a42426c4dfbebcd
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-07 18:26:33 +0600

    Resolve Time Conflictions For A Faculty

--- src/app/modules/OfferedCourse/OfferedCourse.services.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.services.ts
@@ -95,6 +95,34 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     );
   }
 
+  // get the schedules of the faculties
+  const assignedSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    faculty,
+    days: { $in: days },
+  }).select('days startTime endTime');
+
+  // new schedule
+  const newSchedule = {
+    days,
+    startTime,
+    endTime,
+  };
+
+  assignedSchedules.forEach(schedule => {
+    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+
+    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        `This faculty is not available at that time! Choose other time or date!`,
+      );
+    }
+  });
+
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
 };
