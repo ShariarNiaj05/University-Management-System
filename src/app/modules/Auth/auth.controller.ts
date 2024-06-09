c7b3686815e259162dc54cec6ceeb22be9f82eb7
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-10 01:37:20 +0600

    Create Refresh Token When Login And Sent It To Client

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -2,14 +2,24 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { AuthServices } from './auth.services';
+import config from '../../config';
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
+  const { accessToken, refreshToken, needPasswordChange } = result;
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User logged in successfully',
-    data: result,
+    data: {
+      accessToken,
+      refreshToken,
+      needPasswordChange,
+    },
   });
 });
 const changePassword = catchAsync(async (req, res) => {
