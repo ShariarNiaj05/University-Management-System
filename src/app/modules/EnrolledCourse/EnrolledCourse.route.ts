4148e81a557232200002e2e1c226ce60419c1076
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-21 00:02:38 +0600

    updated enrolled course and response type

--- src/app/modules/EnrolledCourse/EnrolledCourse.route.ts
+++ src/app/modules/EnrolledCourse/EnrolledCourse.route.ts
@@ -3,6 +3,7 @@ import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { EnrolledCourseValidations } from './enrolledCourse.validation';
 import { EnrolledCourseControllers } from './enrolledCourse.controller';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = Router();
 
@@ -13,6 +14,12 @@ router.post(
     EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
   ),
   EnrolledCourseControllers.createEnrolledCourse,
+);
+
+router.get(
+  '/my-enrolled-courses',
+  auth(USER_ROLE.student),
+  EnrolledCourseControllers.getMyEnrolledCourses,
 );
 
 router.patch(
