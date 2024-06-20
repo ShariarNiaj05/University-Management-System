8e16f7e499a7cdec08601fcf4e697228b4908396
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-20 15:44:14 +0600

    Add Business Logic Validation Into Enrolled Course Service

--- src/app/modules/EnrolledCourse/EnrolledCourse.services.ts
+++ src/app/modules/EnrolledCourse/EnrolledCourse.services.ts
@@ -1,4 +1,9 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { OfferedCourse } from '../OfferedCourse/OfferedCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
+import EnrolledCourse from './enrolledCourse.model';
+import { Student } from '../student/student.model';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
@@ -9,6 +14,31 @@ const createEnrolledCourseIntoDB = async (
    * step:2 > check if the student is already enrolled for the specific course
    * step:3 > if pass the above validation create an enrolled course
    */
+
+  const { offeredCourse } = payload;
+
+  const isOfferedCourseExist = await OfferedCourse.findById(offeredCourse);
+  if (!isOfferedCourseExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered course not found');
+  }
+
+  if (isOfferedCourseExist.maxCapacity <= 0) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Room is full');
+  }
+
+  const student = Student.findOne({ id: userId }).select('id');
+  if (!student) {
+    throw new AppError(httpStatus.NOT_FOUND, 'student  not found');
+  }
+
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExist.semesterRegistration,
+    offeredCourse,
+    student: student._id,
+  });
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.CONFLICT, 'You are already enrolled');
+  }
 };
 
 const updateEnrolledCourseMarksIntoDB = async () => {};
